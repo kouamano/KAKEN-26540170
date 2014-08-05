989593196699b010acb25c8895ff5502a670f0a9
kouamano <amano.au1@gmail.com>
2014-08-05 14:27:58 +0900

    new

---
+++ BANK/PMC/bin/region-sort_from_sec_pos.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+while(<>){
+	push(@arr,$_);
+}
+$astr = join('',@arr);
+@arr = split(";;;\n",$astr);
+foreach (@arr) {
+	@subarr = split("\n",$_);
+	$fname = shift(@subarr);
+	@subsubarr = ();
+	foreach (@subarr) {
+		if($_ =~ /^[0-9]+\-[0-9]+/){
+			push(@subsubarr,"$_\n");
+		}
+	}
+	@srtarr = sort {$a <=> $b} @subsubarr;
+	print "$fname\n";
+	print @srtarr;
+	print ";;;\n";
+}
