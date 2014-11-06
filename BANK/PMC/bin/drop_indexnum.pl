b1a190058db664ce0311fa38ef0d05450ee026d4
kouamano <amano.au1@gmail.com>
2014-11-06 11:38:15 +0900

    rm

--- BANK/PMC/bin/drop_indexnum.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-while(<>){
-	$_ =~ s/\s+[0-9]+\s+//i;
-	print $_;
-}
--- BANK/PMC/bin/drop_taildot.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-while(<>){
-	chomp;
-	$_ =~ s/\s*\.+\s*$//;
-	$_ =~ s/^\s+//;
-	print "$_\n";
-}
